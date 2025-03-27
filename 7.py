4f1363863c4a608d58f0c2e3d43e319e7dac960a
paulgar26 <paulgar260@gmail.com>
2025-03-27 11:14:28 -0600

    Exercise 7
    Exercise 7

---
+++ 7.py
@@ -0,0 +1,23 @@
+def duncio(listas):
+    lista=['bruno','gaddel','pablo','luis','alan']
+   
+    print("Papas")
+    for item in listas:
+        print(item)
+    print("amigos")
+    for item in lista:
+        print(item)
+
+
+def dunci():
+    
+    listas=['hector hugo garcia santana','Claudia giovanna santana olvera']
+    
+    duncio(listas)
+    
+dunci()
+
+
+   
+
+    
